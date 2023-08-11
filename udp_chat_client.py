02f85d00c160fb8483a9bb75df4bf0db4d7d7f8e
Kunal Rajour <97209296+VictoriousKnight@users.noreply.github.com>
2023-08-12 00:35:19 +0530

    Add files via upload

---
+++ udp_chat_client.py
@@ -0,0 +1,34 @@
+import socket
+import threading
+import random
+
+#creating client socket using IPV4 and UDP
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client_socket.bind((socket.gethostbyname(socket.gethostname()), random.randint(13000, 60000)))
+
+#taking username input
+username = input("Username: ")
+
+#defining recieve function
+def recieve():
+    while True:
+        try:
+            message, _ = client_socket.recvfrom(1024)
+            print(f"{message.decode('utf-8')}")
+            
+        except:
+            pass
+        
+thread = threading.Thread(target=recieve)
+thread.start()
+
+#sending messages to the server
+client_socket.sendto(f"{username} is online...".encode('utf-8'), (socket.gethostbyname(socket.gethostname()), 12345))
+
+while True:
+    message = input("You: ")
+    if message == "!DISCONNECT":
+        client_socket.sendto(f"{username} left the chatroom...".encode('utf-8'), (socket.gethostbyname(socket.gethostname()), 12345))
+        exit()
+    else:
+        client_socket.sendto(f"{username}: {message}".encode('utf-8'), (socket.gethostbyname(socket.gethostname()), 12345))
---
+++ udp_chat_server.py
@@ -0,0 +1,59 @@
+import socket
+import threading
+import queue
+
+#defining clients list using queue data structure
+message_queue = queue.Queue()
+clients_list = []
+
+#creating server socket using IPV4 and UDPss
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.bind((socket.gethostbyname(socket.gethostname()), 12345))
+print("Server is starting...")
+
+#no need to listen to the connections since it is an UDP server
+
+#defining recieve function
+def recieve():
+    while True:
+        try:
+            #recieving data from the clients and inserting the tuple (message, address) into the queue asynchronously
+            message, address = server_socket.recvfrom(1024)
+            #print(message, address)
+            message_queue.put((message, address)) 
+        
+        except:
+            pass
+        
+#defining broadcast function
+def broadcast():
+    while True:
+        #broadcasting only when the queue is not empty
+        while not message_queue.empty():
+            message, address = message_queue.get() #removing messages from the queue
+            print(message.decode("utf-8"))
+            
+            #if the client is not in the client list then add it
+            if address not in clients_list:
+                clients_list.append(address)
+                #print(clients_list)
+                
+            #sending back the data to the client    
+            for client in clients_list:
+                try:
+                    if message.decode('utf-8').endswith("is online..."):
+                        username = message.decode("utf-8")[0:message.decode("utf-8").index("is online...")] #describing the username using string slicing
+                        server_socket.sendto(f"{username} entered the chatroom...".encode("utf-8"), client)
+                        
+                    else:
+                        server_socket.sendto(message, client)
+                        print("test1")
+                        
+                except:
+                    clients_list.remove(client)
+                    
+thread1 = threading.Thread(target=recieve)
+thread2 = threading.Thread(target=broadcast)
+
+thread1.start()
+thread2.start()
